c61dab7f142f6ad6ac0715104b29352d874bdd55
use to run terraform files <terraform@localhost.localdomain>
2024-08-22 15:49:49 -0400

    change security.tf

--- security.tf
+++ security.tf
@@ -1,6 +1,43 @@
 resource "aws_security_group" "ingress-all-test" {
 name = "allow-all-sg"
 vpc_id = "${aws_vpc.test-env.id}"
+  ingress {
+    description = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description = "Allow traffic to nginx server (port 8080 by default)"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 ingress {
     cidr_blocks = [
       "0.0.0.0/0"
@@ -17,3 +54,4 @@ from_port = 22
    cidr_blocks = ["0.0.0.0/0"]
  }
 }
+*/
